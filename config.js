718d004277d148e77a1b73f5a6c47b56fefd03d2
korsan411 <el.korsan@gmail.com>
2025-10-27 05:33:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -8,25 +8,80 @@ const CONFIG = {
     maxPoints3D: 1500,
     maxDebugLogs: 100,
     defaultSettings: {
-        router: { feed: 800, safeZ: 5, maxDepth: 3, stepOver: 5 },
-        laser: { feed: 2000, safeZ: 0, maxDepth: 0, stepOver: 0.2 },
-        threed: { layerHeight: 0.2, fillDensity: 20, printSpeed: 50 }
+        router: { 
+            feed: 800, 
+            safeZ: 5, 
+            maxDepth: 3, 
+            stepOver: 5,
+            workWidth: 30,
+            workHeight: 20
+        },
+        laser: { 
+            feed: 2000, 
+            safeZ: 0, 
+            maxDepth: 0, 
+            stepOver: 0.2,
+            workWidth: 30,
+            workHeight: 20,
+            power: 80,
+            speed: 2000
+        },
+        threed: { 
+            layerHeight: 0.2, 
+            fillDensity: 20, 
+            printSpeed: 50,
+            workWidth: 30,
+            workHeight: 20,
+            workDepth: 10
+        }
     },
     colormaps: ['jet', 'hot', 'cool', 'gray'],
     supportedFormats: {
         images: ['image/jpeg', 'image/png', 'image/gif', 'image/bmp'],
         models3d: ['.stl', '.obj', '.3ds', '.dae', '.ply'],
         vectors: ['.svg', '.dxf']
+    },
+    edgeModes: {
+        router: ['auto', 'sobel', 'laplace'],
+        laser: ['canny', 'adaptive', 'morphological', 'gradient']
+    },
+    simulation: {
+        maxPoints: 1500,
+        defaultSpeed: 1,
+        toolColors: {
+            router: 0xff4444,
+            laser: 0xff4444,
+            threed: 0x10b981
+        }
     }
 };
 
-// Global variables
-let cvReady = false;
-let grayMat = null;
-let contour = null;
-let previewCanvas = null;
-let additionalContours = [];
-let lastScanDir = 'x';
-let lastGeneratedGcode = '';
-let isProcessing = false;
-let currentColormap = 'jet';
+// Global state
+const APP_STATE = {
+    cvReady: false,
+    grayMat: null,
+    contour: null,
+    previewCanvas: null,
+    additionalContours: [],
+    lastScanDir: 'x',
+    lastGeneratedGcode: '',
+    isProcessing: false,
+    currentColormap: 'jet',
+    currentMachine: 'router',
+    threeDModel: null,
+    simulation: {
+        isPlaying: false,
+        animationFrame: null,
+        tool: null,
+        toolPath: null,
+        pathPoints: [],
+        index: 0,
+        speed: 1,
+        elapsedTime: 0
+    }
+};
+
+// Export for use in other modules
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { CONFIG, APP_STATE };
+}
